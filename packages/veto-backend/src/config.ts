c9f2c9d13e813502bcc6fa64780b4e713104e362
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-01-11 17:05:40 -0500

    Add config util Add proxy to serve FE in dev Remove api proxy from FE Add start script to boot boot FE & BE Edit tsconfig.json so we can use ts-node on BE

---
+++ packages/veto-backend/src/config.ts
@@ -0,0 +1,4 @@
+export default {
+    port: process.env.PORT ?? 8080,
+    environment: process.env.NODE_ENV ?? 'dev',
+}
--- packages/veto-backend/src/index.ts
+++ packages/veto-backend/src/index.ts
@@ -1,1 +1,21 @@
+import express, {Request, Response} from "express";
+import proxy from 'express-http-proxy';
+import config from './config';
+
+const app = express();
+
+console.log('config = ', config);
+if (config.environment === 'dev') {
+    app.use('/', proxy('localhost:3000'));
+} else {
+    app.use('/', express.static('frontend'))
+}
+
+app.get('/api', (request: Request, response: Response) => {
+    console.log('request.body', request.body);
+    response.send('Hello World!')
+});
+
+app.listen(config.port, () => console.log(`Example app listening on port ${config.port}!`));
+
 console.log('hello from typescript');
