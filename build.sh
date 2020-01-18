96de0374246b75b84d1ebeb4d9b53911ef318d94
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-01-18 17:34:49 -0500

    Add cyphernode to the project Add a basic development cyphernode config file Update lifecycle script to manage cyphernode
    For now we miss a proper config.7z file to run the setup properly.
    It will be commited next

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+CFG_PASSWORD=${CFG_PASSWORD:-default}
 VERSION_NUMBER=${VERSION_NUMBER:-latest}
 
+echo "Building Veto docker image..."
 docker build . -t veto:${VERSION_NUMBER}
+
+CFG_PASSWORD=$CFG_PASSWORD ./cyphernode/setup.sh -irc
--- run.sh
+++ run.sh
@@ -4,5 +4,15 @@ VERSION_NUMBER=${VERSION_NUMBER:-latest}
 
 ./stop.sh
 
+if [[ -f ./cyphernode/start.sh ]]; then
+  echo 'Cyphernode start script found. Starting cyphernode...'
+  ./cyphernode/start.sh
+else
+  echo 'No cyphernode start script found. Skiping...'
+fi
+
 # Start docker container
+echo 'Starting Veto'
 docker run --init -d -p 80:8080 --name veto veto:${VERSION_NUMBER}
+
+echo 'Veto is now running, it is available at http://localhost'
