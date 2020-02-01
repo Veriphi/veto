1a14de44179e17af20203602328184d21eeea246
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-02-01 17:07:37 -0500

    Fix conditions in bash scripts Also stop cyphernode before running veto Make pre-run.sh cross platform (linux & mac)

--- stop.sh
+++ stop.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
+SKIP_CYPHERNODE=${SKIP_CYPHERNODE:-false}
 VERSION_NUMBER=${VERSION_NUMBER:-latest}
+
+echo "Stopping everything..."
+
+if [[ "${SKIP_CYPHERNODE}" == "true" ]]; then
+  echo 'Skipping Cyphernode...'
+elif [[ -f ./cyphernode/start.sh ]]; then
+  echo 'Cyphernode stop script found. Stopping Cyphernode now...'
+  cd cyphernode/
+  ./stop.sh
+  cd ../
+else
+  echo 'No Cyphernode stop script found. Skiping...'
+fi
 
 # Fetch running Veto instances
 runningVetoInstances=$(docker ps | grep "veto" | awk '{ print $1 }');
