da63ae8a7144be36142ae4e3ba58796af5c8cdb8
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-02-04 18:48:38 -0500

    Generate a new token on each new request made to cyphernode

--- packages/veto-backend/src/api/cyphernode/client.ts
+++ packages/veto-backend/src/api/cyphernode/client.ts
@@ -2,22 +2,16 @@ import axios, { AxiosInstance } from 'axios'
 import https from 'https'
 import crypto from 'crypto'
 import config from '../../utils/config'
-// import * as fs from "fs";
-
 // Create https agent with self signed cyphernode certificate.
 
 //TODO: switch to path from config
 const cyphernodeKey = config.cyphernode.credentials.key
 const cyphernodeKeyId = config.cyphernode.credentials.keyId
 
-// const buffer = fs.readFileSync('../../cyphernode/cacert.pem');
 const buffer = Buffer.from(config.cyphernode.credentials.cert)
-console.log('ze buffer', buffer.toString() === config.cyphernode.credentials.cert)
-// Todo, move to utils file
 const httpsAgent = new https.Agent({
   rejectUnauthorized: false, // (NOTE: this will disable client verification)
   ca: buffer,
-  // ca: Buffer.from(config.cyphernode.credentials.cert),
 })
 
 /**
@@ -26,7 +20,7 @@ const httpsAgent = new https.Agent({
  * TODO: Implement standard JWT signing when cyphernode updates this.
  */
 function getBearerToken(key: string, keyId: string): string {
-  const current = Math.round(new Date().getTime() / 1000) + 60
+  const current = Math.round(new Date().getTime() / 1000) + 10
   const h64 = Buffer.from('{"alg":"HS256","typ":"JWT"}').toString('base64')
   const payload = '{"id":"' + keyId + '","exp":' + current + '}'
   const p64 = Buffer.from(payload).toString('base64')
@@ -47,8 +41,18 @@ export default function makeClient(): AxiosInstance {
       httpsAgent,
       baseURL: 'https://localhost:2009/v0/',
       timeout: 1000,
-      headers: { Authorization: `Bearer ${getBearerToken(cyphernodeKey, cyphernodeKeyId)}` },
     })
+
+    client.interceptors.request.use(
+      (config) => {
+        // Generate a new token for every request
+        config.headers.Authorization = `Bearer ${getBearerToken(cyphernodeKey, cyphernodeKeyId)}`
+        return config
+      },
+      (error) => {
+        Promise.reject(error)
+      },
+    )
   }
 
   return client
