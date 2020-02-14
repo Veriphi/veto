91c130f140d072459718328499e2119f5b831a44
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-02-14 14:29:01 -0500

    Update maintenance-app tsconfig.js file Add nodemon.json to maintenance-app Add wip notice to rockpi-setup/ssd/install.sh Add a testing route to maintenance-app that retrieve the docker version on the host system Add getDockerVersion script to maintenance-app (example of command for other to base their work on) Add a config file to maintenance-app

--- packages/maintenance-app/src/index.ts
+++ packages/maintenance-app/src/index.ts
@@ -1,8 +1,30 @@
 import { Request, Response } from 'express'
-
 import express from 'express'
+import config from './utils/config'
+import getDockerVersion from './scripts/getDockerVersion'
+
 const app = express()
 
-app.get('/version', async (request: Request, response: Response) => {
-  response.send({ name: 'Veto', version: '0.0.0' })
+app.get('/', async (request: Request, response: Response) => {
+  try {
+    const dockerVersion = await getDockerVersion()
+    response.send(`
+            <table>
+                <th>
+                    <td>Software</td>
+                    <td>Version</td>
+                </th>
+                <tr>
+                    <td>Docker version</td>
+                    <td>${dockerVersion}</td>
+                </tr>
+            </table>
+  `)
+  } catch (error) {
+    // TODO: Give proper error handling
+    console.error(error)
+    response.send(error.description)
+  }
 })
+
+app.listen(config.port, () => console.log(`Maintenance-app is now listening on port ${config.port}!`))
