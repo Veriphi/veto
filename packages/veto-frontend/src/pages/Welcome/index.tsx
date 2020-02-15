897a7205441ee82e9343b19fcaf42830d5b684cf
Nathaniel <nat.kitzke@gmail.com>
2020-02-15 13:10:13 -0500

    newer version of veto-ui, use input and button

--- packages/veto-frontend/src/pages/Welcome/index.tsx
+++ packages/veto-frontend/src/pages/Welcome/index.tsx
@@ -1,37 +1,43 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useInput } from '../../hooks/useInput'
-import { mockPassword } from './tempdata.json'
 import { navigate } from '@reach/router'
-import axios from 'axios'
-import { Box, Flex } from '@veriphi/veto-ui'
+import { Flex, Text, Input, Button } from '@veriphi/veto-ui'
+import { mockPassword } from './tempdata.json'
 
 export default () => {
   const password = useInput('')
+
   const handleOnLogin = () => {
     if (password.value === mockPassword) {
       navigate('/dashboard')
     }
   }
 
-  const [data] = useState(null)
-  async function getData() {
-    const data = await axios(`http://${window?.location.host ?? 'localhost'}/api/getmempoolinfo`)
-
-    console.log('\n%cresponse', 'color:orange;font-weight:bold;', data, '\n\n')
-    // setData(await response.json())
-  }
-
   return (
     <Flex sx={{ width: '100vw', height: '100vh' }} flexDirection="column" alignItems="center" justifyContent="center">
-      <h1>Welcome Page</h1>
-      <h3>Please log in with your veto password</h3>
-      <form id="login" onSubmit={handleOnLogin}>
-        <input name="password" type="password" {...password.bind} />
-        <button type="submit">Login</button>
-      </form>
-
-      <button onClick={getData}>Hit the API</button>
-      {data && <div>{data}</div>}
+      <Text m={3} variant="heading1">
+        Welcome Page
+      </Text>
+      <Flex
+        as="form"
+        id="login"
+        onSubmit={handleOnLogin}
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Input
+          my={3}
+          name="password"
+          type="password"
+          placeholder="your node password..."
+          sx={{ minWidth: '20rem' }}
+          {...password.bind}
+        />
+        <Button my={3} type="submit" variant="primary">
+          Login
+        </Button>
+      </Flex>
     </Flex>
   )
 }
