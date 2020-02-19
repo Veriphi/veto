4882c30a7b7a7370238b9dfd4aadfa694519bd31
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-02-18 20:45:23 -0500

    Re-add changes lost due to a bad merge Change workspace rules to packages/shared/*, pacakges/*-app and packages/veto- Add @veto/utils package. It currently hold `config.ts` Add unit test for 'getDockerVersion.ts` Remove direct jest dependency and (we used ts-jest's version instead) Rename build.sh to setup.sh to follow current convention (setup, start, stop) Use setup script for the `yarn cyphernode:setup` command Add packages/ structure to README.md

---
+++ packages/shared/utils/config.ts
@@ -0,0 +1,19 @@
+export enum ENV {
+  dev = 'dev',
+  production = 'production',
+  qa = 'qa',
+}
+
+export type Config = {
+  version: string
+  port: number
+  environment: ENV
+}
+
+export default {
+  // Veto version number
+  version: `0.0.0`,
+  // Veto's listening port
+  port: process.env.PORT ?? 8080,
+  environment: process.env.NODE_ENV ?? 'dev',
+} as Config
