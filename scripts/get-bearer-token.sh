531399c1c87945488f619f6680446b780fbf4ea6
Nathaniel <nat.kitzke@gmail.com>
2020-03-07 11:14:09 -0500

    fix get bearer token script

--- scripts/get-bearer-token.sh
+++ scripts/get-bearer-token.sh
@@ -5,7 +5,7 @@ id='003'
 h64=$(echo -n "{\"alg\":\"HS256\",\"typ\":\"JWT\"}" | base64)
 p64=$(echo -n "{\"id\":\"$id\",\"exp\":$((`date +"%s"`+1000))}" | base64)
 # Get the key that correspond to the keyId
-k=`cat ./cyphernode/keys.txt | grep "${keyId}=" | sed s/$keyId=//`
+k=`cat ./cyphernode/keys.txt | grep "${id}=" | sed s/$id=//`
 s=$(echo -n "$h64.$p64" | openssl dgst -hmac "$k" -sha256 -r | cut -sd ' ' -f1)
 token="$h64.$p64.$s"
 echo $token
