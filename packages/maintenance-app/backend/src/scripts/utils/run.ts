475aa23fdc69ddd7f47e75b9e5def26885d573dd
Damon Perron-Laurin <damonperronlaurin@gmail.com>
2020-05-09 12:59:59 -0400

    Fix unit tests (use proper type for environment variable) Remove console.error because it polutes the unit tests

--- packages/maintenance-app/backend/src/scripts/utils/run.ts
+++ packages/maintenance-app/backend/src/scripts/utils/run.ts
@@ -6,26 +6,19 @@ export type EnvironmentVariables = {
   [name: string]: boolean | string | number
 }
 
-export default function run(script: string, env?: EnvironmentVariables): Promise<string> {
+export default function run(script: string, env?: NodeJS.ProcessEnv): Promise<string> {
   // need to set the current path properly before starting a script
   const pathToScript: string = path.normalize(config.scriptLocation + script)
-  const options = env ? { env: { PATH: process.env.PATH, ...env } } : null
+  const options = env ? { env: { PATH: process.env.PATH, ...env } } : {}
 
   return new Promise((resolve, reject) => {
     execFile(pathToScript, options, (error, stdout, stderr) => {
       if (stderr) {
-        console.error(stderr)
-      }
-
-      if (error) {
-        console.error(error)
+        reject(stderr)
+      } else if (error) {
         reject(error)
-      }
-
-      if (typeof stdout === 'string') {
-        resolve(stdout)
       } else {
-        resolve(stdout.toString())
+        resolve(stdout)
       }
     })
   })
