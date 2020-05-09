0b60aad809ea76f020d05dfa80064b03bd822b70
Nathaniel <nat.kitzke@gmail.com>
2020-05-09 16:36:35 -0400

    set protocol https if production

--- packages/veto-app/frontend/src/api/index.ts
+++ packages/veto-app/frontend/src/api/index.ts
@@ -1,8 +1,11 @@
 import axios from 'axios'
 
+// If we're in production we need to set protocol to https
+const protocol = window.location.host.includes('localhost') ? 'http:' : 'https:'
+
 // URL endpoints (mainly for use by useRequest hook)
-export const balanceUrl = `http://${window.location.host}/api/getbalance`
-export const newAddressUrl = `http://${window.location.host}/api/getnewaddress`
+export const balanceUrl = `${protocol}//${window.location.host}/api/getbalance`
+export const newAddressUrl = `${protocol}//${window.location.host}/api/getnewaddress`
 
 // Calls
 type spendResponse = {
@@ -10,7 +13,7 @@ type spendResponse = {
   hash: 'string'
 }
 export const spend = async ({ address, amount, eventMessage }): Promise<spendResponse> => {
-  const { data }: { data: spendResponse } = await axios.post(`http://${window.location.host}/api/spend`, {
+  const { data }: { data: spendResponse } = await axios.post(`${protocol}//${window.location.host}/api/spend`, {
     address,
     amount,
     eventMessage,
