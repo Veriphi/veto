aeac9997ec024dc7d6f4ad8ce535be08ff12103d
JohannTdl <johanntdl@hotmail.com>
2020-05-10 11:13:46 -0400

    use __webpack_require__

--- packages/veto-app/shared/db/src/gun/index.ts
+++ packages/veto-app/shared/db/src/gun/index.ts
@@ -1,4 +1,7 @@
-if (typeof window !== 'undefined') {
+declare const __webpack_require__: void
+
+// if running in Webpack, take the client
+if (typeof __webpack_require__ === 'function') {
   module.exports = require('./client').default
 } else {
   module.exports = require('./server').default
