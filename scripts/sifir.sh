2e0aaf1e87f10b41f2afe3bf829f0cc08f08c7d8
satoshi <gustavofloresechaiz@gmail.com>
2020-08-05 15:24:08 -0400

    modified config.7z with new versions, tor and lightning

---
+++ scripts/sifir.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+## SETUP 
+
+sudo apt-get install git npm sed
+git clone https://github.com/Sifir-io/cyphernode-app.git
+cd cyphernode-app && ./build.sh
+
+## PLACE KEYS AND CERT
+########## PASSWORD CAN'T BE STATIC
+7z e ~/veto/cyphernode/client.7z -pveriphirocks!
+
+KEY=$(sed -n 's/^.*003=//p' keys.txt)
+ADDRESS=$(cat ~/veto/cyphernode/dist/.cyphernodeconf/tor/traefik/hidden_service/hostname)
+
+touch .env
+
+cat <<EOT >./.env
+CYPHERNODE_API_KEY=${KEY}
+CYPHERNODE_API_KEY_ID=3
+CYPHERNODE_ONION_URL=http://${ADDRESS}:80 
+EOT
+
+rm -r keys.txt 
+
+# RUN 
+
+./run.sh
