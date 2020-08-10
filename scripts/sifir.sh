43750ef97626a730f8ca6da0c6c215de7b1c7bf0
Guillaume <gustavofloresechaiz@gmail.com>
2020-08-10 15:40:08 -0400

    fixed script sifir

--- scripts/sifir.sh
+++ scripts/sifir.sh
@@ -1,19 +1,25 @@
 #/bin/bash
 
-## SETUP 
+CFG_PASSWORD=${CFG_PASSWORD:-"veriphirocks!"}
 
-sudo apt-get install git npm sed
+# SETUP TO ADD TO THE INITIAL SETUP OF EVERYTHING VETO
+
+sudo apt-get install git npm sed -y
+sudo apt remove docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# REPLACE WITH A DOCKER-HUB PULL SOON
 git clone https://github.com/Sifir-io/cyphernode-app.git
 cd cyphernode-app && ./build.sh
 
-## PLACE KEYS AND CERT
-########## PASSWORD CAN'T BE STATIC
-7z e ~/veriphidev/veto/cyphernode/client.7z -pveriphirocks!
-
-KEY=$(sed -n 's/^.*003=//p' keys.txt)
-ADDRESS=$(cat ~/veriphidev/veto/cyphernode/dist/.cyphernodeconf/tor/traefik/hidden_service/hostname)
-
+# CONFIG FILE & VARIABLES
+7z e ~/veriphidev/veto/cyphernode/client.7z -p${CFG_PASSWORD}
 touch .env
+
+ADDRESS=$(cat ~/veriphidev/veto/cyphernode/.cyphernodeconf/tor/traefik/hidden_service/hostname)
+KEY=$(sed -n 's/^.*003=//p' keys.txt)
 
 cat <<EOT >./.env
 CYPHERNODE_API_KEY=${KEY}
@@ -24,5 +30,5 @@ EOT
 rm -r keys.txt 
 
 # RUN 
-echo 'success on Sifir'
+echo 'success on Sifir' 
 ./run.sh
